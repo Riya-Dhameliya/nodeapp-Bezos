8175404af569dc623f3851ec019dd3a2b484a608
Riya <riya@your.hostname.com>
2022-01-20 16:52:08 +0530

    Status changed

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send("<h1 style="color:Yellow;">Hey Riya, Pipeline built successfully!...- version-4!!</h1> \n");
+  res.send('<h1 style="color:Yellow;">Hey Riya!...- Test version-4!!</h1> \n');
 });
 
 app.listen(PORT, HOST);
